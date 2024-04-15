28d383328ca71d2a6da0d236d63b3f5defc8207a
Divyanshu <trucoder786@gmail.com>
2024-04-15 23:30:22 +0530

    Adding More Tables

---
+++ Backend/Event-Man/src/main/java/com/EventBackEnd/EventMan/Entities/Event_details.java
@@ -0,0 +1,28 @@
+package com.EventBackEnd.EventMan.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Event_details {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String event_name;
+    Date date_created;
+    Date event_date;
+    String description;
+    String event_type;
+    String club_host;
+    String event_images;
+}
