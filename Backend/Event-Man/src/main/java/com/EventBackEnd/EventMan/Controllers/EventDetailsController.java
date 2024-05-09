b97cf79f0e2c015aebb3e3cf2cc941d13af0d929
Divyanshu <trucoder786@gmail.com>
2024-05-10 00:58:31 +0530

    Securities

--- Backend/Event-Man/src/main/java/com/EventBackEnd/EventMan/Controllers/EventDetailsController.java
+++ Backend/Event-Man/src/main/java/com/EventBackEnd/EventMan/Controllers/EventDetailsController.java
@@ -3,6 +3,7 @@ package com.EventBackEnd.EventMan.Controllers;
 import com.EventBackEnd.EventMan.Entities.Event_details;
 import com.EventBackEnd.EventMan.Logic.EventDetailsLogic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/avlokan")
+@CrossOrigin("*")
 public class EventDetailsController {
     @Autowired
     private EventDetailsLogic eventDetailsLogic;
--- Backend/Event-Man/src/main/java/com/EventBackEnd/EventMan/Controllers/collegeUserController.java
+++ Backend/Event-Man/src/main/java/com/EventBackEnd/EventMan/Controllers/collegeUserController.java
@@ -5,13 +5,11 @@ import com.EventBackEnd.EventMan.Entities.LoginStudent;
 import com.EventBackEnd.EventMan.Logic.CollegeUserLogic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/avlokan")
+@CrossOrigin("*")
 public class collegeUserController {
     @Autowired
     private CollegeUserLogic collegeUserLogic;
