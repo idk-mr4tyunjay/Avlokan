79466c2f88c1a8521238fb02845e6fb4f8daa7cd
Divyanshu <trucoder786@gmail.com>
2024-05-16 00:24:52 +0530

    Revert "Securities"
    This reverts commit b97cf79f0e2c015aebb3e3cf2cc941d13af0d929.

--- Backend/Event-Man/src/main/java/com/EventBackEnd/EventMan/Controllers/ClubDataController.java
+++ Backend/Event-Man/src/main/java/com/EventBackEnd/EventMan/Controllers/ClubDataController.java
@@ -4,14 +4,12 @@ import com.EventBackEnd.EventMan.Entities.Club_data;
 import com.EventBackEnd.EventMan.Logic.ClubDataLogic;
 import com.EventBackEnd.EventMan.Repository.ClubDataRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/avlokan")
-@CrossOrigin("*")
 public class ClubDataController {
     @Autowired
     private ClubDataLogic clubDataLogic;
--- Backend/Event-Man/src/main/java/com/EventBackEnd/EventMan/Controllers/EventDetailsController.java
+++ Backend/Event-Man/src/main/java/com/EventBackEnd/EventMan/Controllers/EventDetailsController.java
@@ -3,7 +3,6 @@ package com.EventBackEnd.EventMan.Controllers;
 import com.EventBackEnd.EventMan.Entities.Event_details;
 import com.EventBackEnd.EventMan.Logic.EventDetailsLogic;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/avlokan")
-@CrossOrigin("*")
 public class EventDetailsController {
     @Autowired
     private EventDetailsLogic eventDetailsLogic;
